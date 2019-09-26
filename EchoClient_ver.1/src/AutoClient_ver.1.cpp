66ebf8a5748395fbcf8186e2776b2505b476fa00
leejinsoo92 <wlstn4020@naver.com>
2019-09-26 16:07:07 +0900

    Client Send Recv Fixed

--- EchoClient_ver.1/src/AutoClient_ver.1.cpp
+++ EchoClient_ver.1/src/AutoClient_ver.1.cpp
@@ -27,7 +27,7 @@ using namespace std;
 
 //8080
 //9190
-#define DEFAULT_PORT	9190
+#define DEFAULT_PORT	8080
 #define MAX_CONN		16
 #define MAX_EVENTS	32
 #define BUF_SIZE		16
@@ -36,6 +36,10 @@ using namespace std;
 //#define SERVER_IP		"127.0.0.1"
 
 void client_run();
+bool PacketCheck(PACKET data);
+static void set_sockaddr(struct sockaddr_in *addr);
+int RecvFromServer(int sockfd, char* buf, int buf_size);
+bool PacketCheck(PACKET data);
 
 int main()
 {
@@ -43,35 +47,6 @@ int main()
 
 	return 0;
 }
-
-static void set_sockaddr(struct sockaddr_in *addr)
-{
-	bzero((char*)addr, sizeof(struct sockaddr_in));
-	addr->sin_family	= AF_INET;
-	addr->sin_addr.s_addr = inet_addr(SERVER_IP);//INADDR_ANY;inet_addr(SERVER_IP);
-	addr->sin_port = htons(DEFAULT_PORT);
-}
-
-int RecvFromServer(int sockfd, char* buf, int buf_size)
-{
-	int n, offset = 0;
-	errno = 0;
-
-	while(buf_size - offset > 0 && (n = recv(sockfd, buf + offset, buf_size - offset, MSG_DONTWAIT)) > 0)
-	{
-		offset += n;
-	}
-
-	//&& errno == EAGAIN)
-	if( offset == 0 && errno == EAGAIN )
-	{
-		//cout << "[Client] no message recived." << endl;
-		return -1;
-	}
-	else
-		return offset;
-}
-
 
 void client_run()
 {
@@ -82,6 +57,8 @@ void client_run()
 	string loginTemp = "login request";
 	string printTemp = "print request";
 	unsigned short nMemoCmd = 0;
+
+	int randSize = 0;
 
 	PACKET Packet;
 
@@ -103,16 +80,16 @@ void client_run()
 	iData_Len = strlen(Packet.m_szID) - 1;
 	Packet.m_szID[iData_Len] = '\0';
 
-	Packet.m_nCMD ^= Packet.m_nCMD;
+	//Packet.m_nCMD ^= Packet.m_nCMD;
 	Packet.m_nCMD |= CMD_USER_LOGIN_REQ;
-
-
-	memcpy(Packet.m_szData, loginTemp.c_str(), loginTemp.length());
+	strcpy(Packet.m_szData, loginTemp.c_str());
 
 	Packet.m_iSize = loginTemp.length();
 	//cout << "data size : " << Packet.m_iSize << endl;
 
 	send(sockfd, (char*)&Packet, sizeof(Packet), 0);
+	cout << "success send about login data!" << endl;
+	//memset(&Packet.m_szData, 0, sizeof(MAX_PACKET_SIZE));
 
 //	if((numbytes = RecvFromServer(sockfd, (char*)&Packet, sizeof(PACKET))) == 0)
 //	{
@@ -127,52 +104,66 @@ void client_run()
 		int iMenu = 0;
 		string sInput;
 
-		usleep(400000);
-		if ((numbytes = RecvFromServer(sockfd, (char*) &Packet, sizeof(PACKET)))
-				== 0) {
-			cout << "[Client] Server Terminated." << endl;
-			return;
-		}
+		//usleep(1000000);
+		memset(&Packet.m_szData, 0, sizeof(MAX_PACKET_SIZE));
+		//RecvFromServer(sockfd, (char*) &Packet, sizeof(PACKET));
+//		if ((numbytes = RecvFromServer(sockfd, (char*) &Packet, sizeof(PACKET)))
+//				== 0) {
+//			cout << "[Client] Server Terminated." << endl;
+//			return;
+//		}
+		recv(sockfd, (char*)&Packet, sizeof(PACKET), 0);
 
-		if ((Packet.m_nCMD == CMD_USER_LOGIN_RESULT)) {
-			cout << "Login Complete!" << endl;
-			//isSend = false;
-		} else if ((Packet.m_nCMD == CMD_USER_DATA_RESULT)) {
-
-			cout << "[" << Packet.m_nSock_ID << "]" << "echo  : "
-					<< Packet.m_szData << endl;
-			//isSend = false;
-		} else if ((Packet.m_nCMD == CMD_USER_SAVE_RESULT)) {
-			cout << "Save Compelete" << endl;
-			//isSend = false;
-		} else if ((Packet.m_nCMD == CMD_USER_DELETE_RESULT)) {
-			cout << "Delete Compelete" << endl;
-			//isSend = false;
-		} else if ((Packet.m_nCMD == CMD_USER_PRINT_RESULT)) {
-			if (strcmp(Packet.m_szData, "END") == 0)
-				isSend = false;
-			else
-				cout << "[" << Packet.m_nSock_ID << "]" << "	 struct  : "
-						<< Packet.m_szData << endl;
-
-		} else if ((Packet.m_nCMD == CMD_USER_ERR)) {
-			if (nMemoCmd == CMD_USER_DATA_REQ) {
-				cout << "Echo Error" << endl;
-			} else if (nMemoCmd == CMD_USER_SAVE_REQ) {
-				cout << "Save Error" << endl;
-			} else if (nMemoCmd == CMD_USER_DELETE_REQ) {
-				cout << "Delete Error" << endl;
-			} else if (nMemoCmd == CMD_USER_PRINT_REQ) {
-				cout << "Print Error" << endl;
+		//if( true == PacketCheck(Packet))
+		//{
+			if ((Packet.m_nCMD == CMD_USER_LOGIN_RESULT)) {
+				cout << "Login Complete!" << endl;
 			}
-			nMemoCmd = 0;
-			//isSend = false;
-		}
-		usleep(400000);
+			else if ((Packet.m_nCMD == CMD_USER_DATA_RESULT)) {
+				cout << "[" << Packet.m_nSock_ID << "]" << "echo  : " << Packet.m_szData << endl;
+			}
+			else if ((Packet.m_nCMD == CMD_USER_SAVE_RESULT)) {
+				cout << "Save Compelete" << endl;
+				//isSend = false;
+			}
+			else if ((Packet.m_nCMD == CMD_USER_DELETE_RESULT)) {
+				cout << "Delete Compelete" << endl;
+				//isSend = false;
+			}
+			else if ((Packet.m_nCMD == CMD_USER_PRINT_RESULT)) {
+				if (strcmp(Packet.m_szData, "END") == 0)
+					isSend = false;
+				else
+					cout << "[" << Packet.m_nSock_ID << "]" << "	 struct  : " << Packet.m_szData << endl;
 
-		if(isSend == false )
-		{
+			}
+			else if ((Packet.m_nCMD == CMD_USER_ERR)) {
+				if (nMemoCmd == CMD_USER_DATA_REQ) {
+					cout << "Echo Error" << endl;
+				}
+				else if (nMemoCmd == CMD_USER_SAVE_REQ) {
+					cout << "Save Error" << endl;
+				}
+				else if (nMemoCmd == CMD_USER_DELETE_REQ) {
+					cout << "Delete Error" << endl;
+				}
+				else if (nMemoCmd == CMD_USER_PRINT_REQ) {
+					cout << "Print Error" << endl;
+					isSend = false;
+				}
+				nMemoCmd = 0;
+				//isSend = false;
+			}
+			//isSend = false;
+		//}
+
+
+		usleep(1000000);
+
+		//if(isSend == false )
+		//{
 			memset(&Packet.m_szData, 0, sizeof(MAX_PACKET_SIZE));
+			randSize = rand() % 5 + 3;
 			RETURN:
 
 			cout << "--------------" << endl;
@@ -200,32 +191,37 @@ void client_run()
 			{
 			case 1:
 				cout << "Echo input : ";
-//				for(int i = 0; i < 10; ++i)
-//				{
-//					sInput += rand() % 25 + 65;
-//				}
-				sInput = rand() % 25 + 65;
+				for(int i = 0; i < randSize; ++i)
+				{
+					sInput += rand() % 25 + 65;
+				}
+				//sInput = rand() % 25 + 65;
 				cout << sInput << endl;
 
 				Packet.m_nCMD ^= Packet.m_nCMD;
 				Packet.m_nCMD |= CMD_USER_DATA_REQ;
 				nMemoCmd = CMD_USER_DATA_REQ;
-				memcpy(Packet.m_szData, sInput.c_str(), sInput.length());
+
+				strcpy(Packet.m_szData, sInput.c_str());
 				Packet.m_iSize = sInput.length();
-				//cout << "packet data size : " << Packet.m_iSize << endl;
+				Packet.m_szData[Packet.m_iSize] = '\0';
 
 				send(sockfd, (char*) &Packet, sizeof(PACKET), 0);
 
 				break;
 			case 2:
 				cout << "Save input : ";
-				sInput = rand() % 25 + 65;
+				for(int i = 0; i < randSize; ++i)
+				{
+					sInput += rand() % 25 + 65;
+				}
+				//sInput = rand() % 25 + 65;
 				cout << sInput << endl;
 
 				Packet.m_nCMD ^= Packet.m_nCMD;
 				Packet.m_nCMD |= CMD_USER_SAVE_REQ;
 				nMemoCmd = CMD_USER_SAVE_REQ;
-				memcpy(Packet.m_szData, sInput.c_str(), sInput.length());
+				strcpy(Packet.m_szData, sInput.c_str());
 				Packet.m_iSize = sInput.length();
 				//cout << "packet data size : " << Packet.m_iSize << endl;
 
@@ -234,13 +230,16 @@ void client_run()
 				break;
 			case 3:
 				cout << "Delete input : ";
-				sInput = rand() % 25 + 65;
+				for (int i = 0; i < randSize; ++i) {
+					sInput += rand() % 25 + 65;
+				}
+				//sInput = rand() % 25 + 65;
 				cout << sInput << endl;
 
 				Packet.m_nCMD ^= Packet.m_nCMD;
 				Packet.m_nCMD |= CMD_USER_DELETE_REQ;
 				nMemoCmd = CMD_USER_DELETE_REQ;
-				memcpy(Packet.m_szData, sInput.c_str(), sInput.length());
+				strcpy(Packet.m_szData, sInput.c_str());
 				Packet.m_iSize = sInput.length();
 				//cout << "packet data size : " << Packet.m_iSize << endl;
 
@@ -252,10 +251,10 @@ void client_run()
 				Packet.m_nCMD |= CMD_USER_PRINT_REQ;
 				nMemoCmd = CMD_USER_PRINT_REQ;
 
-				memcpy(Packet.m_szData, printTemp.c_str(), printTemp.length());
+				strcpy(Packet.m_szData, printTemp.c_str());
 				Packet.m_iSize = printTemp.length();
 				//cout << "packet data size : " << Packet.m_iSize << endl;
-
+				cout << "Ask Struct Data to Client~" << endl;
 				send(sockfd, (char*) &Packet, sizeof(PACKET), 0);
 				isSend = true;
 				break;
@@ -267,17 +266,69 @@ void client_run()
 
 			//isSend = true;
 
-		}
-//		else
-//		{
-//			//errno != EAGAIN &&
-//
-//
-//		}
+		//}
+
 
 	}
 	close(sockfd);
 }
 
+static void set_sockaddr(struct sockaddr_in *addr)
+{
+	bzero((char*)addr, sizeof(struct sockaddr_in));
+	addr->sin_family	= AF_INET;
+	addr->sin_addr.s_addr = inet_addr(SERVER_IP);//INADDR_ANY;inet_addr(SERVER_IP);
+	addr->sin_port = htons(DEFAULT_PORT);
+}
 
+//int RecvFromServer(int sockfd, char* buf, int buf_size)
+//{
+//	int n, offset = 0;
+//	errno = 0;
+//
+//	//MSG_DONTWAIT
+//	while(buf_size - offset > 0 && (n = recv(sockfd, buf + offset , buf_size - offset, 0)) > 0)
+//	{
+//		offset += n;
+//	}
+//
+//	//&& errno == EAGAIN)
+//	if( offset == 0 && errno == EAGAIN )
+//	{
+//		//cout << "[Client] no message recived." << endl;
+//		return -1;
+//	}
+//	else
+//		return offset;
+//}
+
+//int RecvFromServer(int sockfd, char* buf, int buf_size)
+//{
+//	int iRecvLen = 0;
+//	while (1) {
+//		iRecvLen = recv(sockfd, buf, buf_size, 0);
+//
+//		if (iRecvLen == 0)
+//			return -1;
+//		else if (iRecvLen < 0) {
+//			if ( errno == EAGAIN)
+//				break;
+//		}
+//	}
+//	return 0;
+//}
+
+bool PacketCheck(PACKET data)
+{
+	if( strcmp( data.m_szHead, "AA11" ) != 0 )
+		return false;
+	if( strcmp( data.m_szTail, "11AA" ) != 0 )
+		return false;
+	if( data.m_nCMD <= 0 || data.m_nCMD >= 11)
+		return false;
+	if( data.m_iSize <= 0 )
+		return false;
+
+	return true;
+}
 
