31a0315acfd3732575892b1638cd7ee6e3d41b1b
leejinsoo92 <wlstn4020@naver.com>
2019-10-08 18:27:46 +0900

    Print recv 하는 방법 수정 - 데이터 큰 버퍼에 한번에 받아오는 방법

--- EchoClient_ver.2/src/AutoClient_ver.2.cpp
+++ EchoClient_ver.2/src/AutoClient_ver.2.cpp
@@ -21,20 +21,38 @@
 #include <time.h>
 #include <string.h>
 #include "CircularBuf.h"
+//#include "Packet_Define.h"
 
 #include <iostream>
 using namespace std;
 
+//8080
+//9190
 #define DEFAULT_PORT	8080
 #define MAX_CONN		16
 #define MAX_EVENTS	32
 #define BUF_SIZE		16
 #define MAX_LINE		256
 #define SERVER_IP		"192.168.8.39"
+//#define SERVER_IP		"127.0.0.1"
+
+struct CIR_BUF
+{
+	char szTempBuf[1024 * 32];
+	int iFront = 0;
+	int iRear = 0;
+	int iMaxSize = 0;
+
+	CIR_BUF()
+	{
+		iMaxSize = 1024 * 32;
+	}
+};
 
 void client_run();
 static void set_sockaddr(struct sockaddr_in *addr);
 int Set_NonBlocking(int iSockfd);
+int RecvFromServer(int sockfd, char* buf, int buf_size, CIR_BUF* pCirBuf);
 
 int main()
 {
@@ -48,9 +66,7 @@ void client_run()
 	int sockfd = 0;
 	int iData_Len = 0;
 	struct sockaddr_in srv_addr;
-	int iOpenMenu = 0;
-	bool isAuto = true;
-	bool isPrintFinish = false;
+	bool isAuto = false;
 
 	string loginTemp = "login request";
 	string printTemp = "print request";
@@ -61,6 +77,7 @@ void client_run()
 
 	CCircularBuf* m_pCirBuf = new CCircularBuf();
 
+	//CIR_BUF pCirBuf;
 	char szRecvBuf[MAX_PACKET_SIZE] = { } ;
 
 	srand(time(NULL));
@@ -76,30 +93,16 @@ void client_run()
 	}
 
 	if(fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFD, 0) | O_NONBLOCK) == -1)
-		cout << "error" << endl;
+			cout << "error" << endl;
 
-
-	cout << "===========================" << endl;
-	cout << "	Echo Program" << endl;
-	cout << endl;
-	cout << "	1. 직접 입력" << endl;
-	cout << "	2. 자동 입력" << endl;
-	cout << "===========================" << endl;
-	cout << "select : ";
-	cin >> iOpenMenu;
-
-	if(iOpenMenu == 1)
-		isAuto = false;
-	else if(iOpenMenu == 2)
-		isAuto = true;
-
-	cin.ignore();
 
 	PACKET_CS_LOGIN Login_Packet;
 	cout << "ID : ";
 	fgets(Login_Packet.m_szID, sizeof(Login_Packet.m_szID), stdin);
 	iData_Len = strlen(Login_Packet.m_szID) - 1;
 
+//	sID = "TEST";
+//	strcpy(Login_Packet.m_szID, sID.c_str());
 	Login_Packet.m_szID[iData_Len] = '\0';
 
 	send(sockfd, (char*)&Login_Packet, sizeof(PACKET_CS_LOGIN), 0);
@@ -131,11 +134,27 @@ void client_run()
 				{
 					m_pCirBuf->Push( szRecvBuf, iRecvLen );
 
+//					if( pCirBuf.iMaxSize < pCirBuf.iRear + iRecvLen )
+//					{
+//						int iDivide = pCirBuf.iMaxSize - pCirBuf.iRear;
+//						memcpy(&pCirBuf.szTempBuf[pCirBuf.iRear], szRecvBuf, iDivide);
+//
+//						pCirBuf.iRear = iRecvLen - iDivide;
+//						memcpy(pCirBuf.szTempBuf, szRecvBuf + iDivide, pCirBuf.iRear);
+//					}
+//					else
+//					{
+//						memcpy(&pCirBuf.szTempBuf[pCirBuf.iRear], szRecvBuf, iRecvLen);
+//						pCirBuf.iRear += iRecvLen;
+//					}
 				}
 			}
 
+			//head = (PACKET_HEAD*)(&pCirBuf.szTempBuf[pCirBuf.iFront]);
 			head = (PACKET_HEAD*)m_pCirBuf->GetPacket(sizeof(PACKET_HEAD));
 			PACKET_TAIL* tail = (PACKET_TAIL*)(head + head->m_iPacketSize - sizeof(PACKET_TAIL));
+			//PACKET* packet = (PACKET*)(head);
+
 
 			if( m_pCirBuf->isEmpty() )
 			{
@@ -148,10 +167,20 @@ void client_run()
 				goto RECV_BACK;
 			}
 
+
+			//strncmp(tail->m_szTail, gpTail, sizeof(gpTail)) != 0
 			if( strncmp(head->m_szHead, gpHead, sizeof(gpHead)) != 0 )
 			{
 				m_pCirBuf->Pop( iDataSize );
+
+//				if( pCirBuf.iMaxSize < pCirBuf.iFront + head->m_iPacketSize )
+//					pCirBuf.iFront = head->m_iPacketSize - ( pCirBuf.iMaxSize - pCirBuf.iFront );
+//				else
+//					pCirBuf.iFront += head->m_iPacketSize;
+
+
 				goto RECV_BACK;
+				//isSend = false;
 			}
 
 
@@ -162,7 +191,6 @@ void client_run()
 				PACKET_SC_LOGIN* packet = (PACKET_SC_LOGIN*)(head);
 				cout << "Login Complete!" << endl << endl;
 				isSend = false;
-
 			}
 			break;
 			case CMD_USER_DATA_RESULT:
@@ -170,7 +198,6 @@ void client_run()
 				PACKET_SC_ECHO* packet = (PACKET_SC_ECHO*)(head);
 				cout << "echo  : " << packet->m_szData << endl<< endl;
 				isSend = false;
-
 			}
 			break;
 			case CMD_USER_SAVE_RESULT:
@@ -205,73 +232,38 @@ void client_run()
 				PACKET_SC_PRINT* packet = (PACKET_SC_PRINT*)(head);
 				if( true == packet->m_isComplete )
 				{
-					int iCnt = 0;
-					int iStringCnt = 0;
-					while (1) {
-						if (packet->m_szData[iCnt] == '\0') {
-							++iStringCnt;
-							cout << endl;
-						} else
-							cout << packet->m_szData[iCnt];
-
-						if (iStringCnt >= packet->m_iPrintCnt)
-							break;
-						++iCnt;
-					}
-
-					//cout << "Reposit  : " << packet->m_szData << endl;
-//					isPrintFinish = false;
-//					isSend = false;
-//					m_pCirBuf->Pop(head->m_iPacketSize);
-
 					cout << endl;
 					cout << "<< Print Finish! >>" << endl;
 					cout << endl;
 					isSend = false;
-					isPrintFinish = true;
 				}
 				else
 				{
-					int iCnt = 0;
-					int iStringCnt = 0;
-					while(1)
-					{
-						if( packet->m_szData[iCnt] == '\0')
-						{
-							++iStringCnt;
-							cout << endl;
-						}
-						else
-							cout << packet->m_szData[iCnt];
-
-						if(iStringCnt >= packet->m_iPrintCnt)
-							break;
-						++iCnt;
-					}
-
-
-					//cout << "Reposit  : " << packet->m_szData << endl;
-					isPrintFinish = false;
-					isSend = false;
-					m_pCirBuf->Pop( head->m_iPacketSize );
-					//usleep(100000);
-					goto RE_PRINT;
+					cout << "Reposit  : " << packet->m_szData << endl;
 				}
 
 			}
 			break;
 			}
+
+
 			m_pCirBuf->Pop( head->m_iPacketSize );
 
+//			if( pCirBuf.iMaxSize < pCirBuf.iFront + head->m_iPacketSize )
+//				pCirBuf.iFront = head->m_iPacketSize - ( pCirBuf.iMaxSize - pCirBuf.iFront );
+//			else
+//				pCirBuf.iFront += head->m_iPacketSize;
+
 		}
+
+
 
 		//system("clear");
 		if(isSend == false )
 		{
-			if(isAuto == true)
-				usleep(100000);
-
-			randSize = rand() % 35 + 3;
+			usleep(100000);
+			//memset(&Packet.m_szData, 0, sizeof(MAX_PACKET_SIZE));
+			randSize = 1;//rand() % 5 + 3;
 			RETURN:
 
 			cout << "--------------" << endl;
@@ -284,14 +276,9 @@ void client_run()
 			cout << "--------------" << endl;
 			cout << "select : ";
 
-			if(true == isAuto)
-			{
-				iMenu = rand() % 4 + 1;
-			}
-			else
-				cin >> iMenu;
+			iMenu = rand() % 4 + 1;
+			cout << iMenu << endl;
 
-			cout << endl;
 			if( iMenu > 5 || iMenu < 1)
 			{
 				cin.clear();
@@ -305,20 +292,12 @@ void client_run()
 			case 1:
 			{
 				cout << "Echo input : ";
-
-				if(true == isAuto)
+				for(int i = 0; i < randSize; ++i)
 				{
-					for(int i = 0; i < randSize; ++i)
-					{
-						sInput += rand() % 25 + 65;
-					}
-					cout << sInput << endl;
+					sInput += rand() % 25 + 65;
 				}
-				else
-				{
-					cin.ignore(256, '\n');
-					getline(cin, sInput);
-				}
+				//sInput = rand() % 25 + 65;
+				cout << sInput << endl;
 
 				PACKET_CS_ECHO packet;
 
@@ -334,16 +313,13 @@ void client_run()
 			case 2:
 			{
 				cout << "Save input : ";
-				if (true == isAuto) {
-					for (int i = 0; i < randSize; ++i) {
-						sInput += rand() % 25 + 65;
-					}
-					cout << sInput << endl;
-				} else {
-					cin.ignore(256, '\n');
-					getline(cin, sInput);
+				for(int i = 0; i < randSize; ++i)
+				{
+					sInput += rand() % 25 + 65;
 				}
-				cout << endl;
+				//sInput = rand() % 25 + 65;
+				cout << sInput << endl;
+
 				PACKET_CS_SAVE packet;
 
 				strncpy(packet.m_szData, sInput.c_str(), sizeof(packet.m_szData));
@@ -357,16 +333,12 @@ void client_run()
 			case 3:
 			{
 				cout << "Delete input : ";
-				if (true == isAuto) {
-					for (int i = 0; i < randSize; ++i) {
-						sInput += rand() % 25 + 65;
-					}
-					cout << sInput << endl;
-				} else {
-					cin.ignore(256, '\n');
-					getline(cin, sInput);
+				for (int i = 0; i < randSize; ++i) {
+					sInput += rand() % 25 + 65;
 				}
-				cout << endl;
+				//sInput = rand() % 25 + 65;
+				cout << sInput << endl;
+
 				PACKET_CS_DEL packet;
 
 				strncpy(packet.m_szData, sInput.c_str(), sizeof(packet.m_szData));
@@ -379,8 +351,9 @@ void client_run()
 				break;
 			case 4:
 			{
-				RE_PRINT:
 				PACKET_CS_PRINT packet;
+
+				cout << "Ask Struct Data to Client~" << endl;
 
 				send(sockfd, (char*) &packet, sizeof(PACKET_CS_PRINT), 0);
 				isSend = true;
@@ -403,7 +376,7 @@ static void set_sockaddr(struct sockaddr_in *addr)
 {
 	bzero((char*)addr, sizeof(struct sockaddr_in));
 	addr->sin_family	= AF_INET;
-	addr->sin_addr.s_addr = inet_addr(SERVER_IP);
+	addr->sin_addr.s_addr = inet_addr(SERVER_IP);//INADDR_ANY;inet_addr(SERVER_IP);
 	addr->sin_port = htons(DEFAULT_PORT);
 }
 
@@ -411,6 +384,32 @@ int Set_NonBlocking(int iSockfd)
 {
 	if(fcntl(iSockfd, F_SETFL, fcntl(iSockfd, F_GETFD, 0) | O_NONBLOCK) == -1)
 		return -1;
+	return 0;
+}
+
+int RecvFromServer(int sockfd, char* buf, int buf_size, CIR_BUF* pCirBuf)
+{
+	int iRecvLen = 0;
+	char recvBuf[MAX_PACKET_SIZE];
+	while (1) {
+		iRecvLen = recv(sockfd, recvBuf, sizeof(recvBuf), 0);
+
+		if (iRecvLen == 0)
+			return -1;
+		else if (iRecvLen <= 0)
+		{
+			if ( errno == EAGAIN)
+				break;
+		}
+		else
+		{
+			if(pCirBuf->iRear + iRecvLen > pCirBuf->iMaxSize)
+				pCirBuf->iRear = 0;
+
+			memcpy( &pCirBuf->szTempBuf[pCirBuf->iRear], recvBuf, iRecvLen);
+			pCirBuf->iRear += iRecvLen;
+		}
+	}
 	return 0;
 }
 
